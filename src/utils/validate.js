4c798fccc004065ba158e27417ab256b96c73925
wylwq <1928104761@qq.com>
2020-03-03 23:23:36 +0800

    登录的验证完善

---
+++ src/utils/validate.js
@@ -0,0 +1,12 @@
+/**
+ * 过滤特殊字符
+ */
+export function validusername(value) {
+     let reg = /^1[3456789]\d{9}$/;
+     return reg.test(value) ? true :false;
+}
+
+export function validpassword(value) {
+    let reg = /^\w{6,20}$/;
+    return reg.test(value) ? true : false;
+}
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,20 +1,179 @@
 <template>
   <div id="login">
-    这是登陆页面，我的第一个vue程序<el-button>默认按钮</el-button><el-button type="primary">主要按钮</el-button>
-  <el-button type="success">成功按钮</el-button>
-  <el-button type="info">信息按钮</el-button>
-  <el-button type="warning">警告按钮</el-button>
-  <el-button type="danger">危险按钮</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          :class="{'current': item.current}"
+          v-for="item in menutab"
+          :key="item.id"
+          @click="toggleMenu(item)"
+        >{{item.txt}}</li>
+      </ul>
+      <!-- 登录表单 -->
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="login-form" size="medium">
+        <el-form-item prop="username">
+          <label>账号</label>
+          <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <label>密码</label>
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength='6' maxlength='20'></el-input>
+        </el-form-item>
+        <el-form-item prop="passwords" v-show="model === 'register'">
+          <label>重复密码</label>
+          <el-input type="password" v-model="ruleForm.passwords" autocomplete="off" minlength='6' maxlength='20'></el-input>
+        </el-form-item>
+        <el-form-item prop="code" v-show="model === 'register'">
+          <label>验证码</label>
+          <el-row :gutter="20">
+            <el-col :span="15"><el-input v-model.number="ruleForm.code"></el-input></el-col>
+            <el-col :span="9"><el-button type="success" class="block">获取验证码</el-button></el-col>
+          </el-row>
+          
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')" class="block">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 <script>
+import {validusername, validpassword} from '@/utils/validate'
 export default {
-    
-}
+  name: "login",
+  data() {
+    var checkCode = (rule, value, callback) => {
+      if(this.model === 'login') {
+      callback();
+    }
+    if (!value) {
+        return callback(new Error('验证码不能为空'));
+    }
+    setTimeout(() => {
+        if (!Number.isInteger(value)) {
+        callback(new Error('请输入数字值'));
+        }else {
+            callback();
+        }
+    }, 1000);
+    };
+    var validateUsername = (rule, value, callback) => {
+    if (value === '') {
+        callback(new Error('请输入手机号'));
+    } else if(validusername(value)) {
+        callback(new Error('请输入正确的手机号'));
+    } else {
+        callback();
+    }
+    };
+    var validatePassword = (rule, value, callback) => {
+    if (value === '') {
+        callback(new Error('请输入密码'));
+    } else if(validpassword(value)){
+        callback(new Error('密码为6-20的数字加字母'));
+    } else {
+        callback();
+    }
+    };
+    var validatePasswords = (rule, value, callback) => {
+    if(this.model === 'login') {
+      callback();
+    }
+    if (value === '') {
+        callback(new Error('请在次输入密码'));
+    } else if(value != this.ruleForm.password){
+        callback(new Error('两次输入的密码不一样'));
+    } else {
+        callback();
+    }
+    };
+    return {
+    menutab: [
+        { txt: "登录", current: false, type: 'login' },
+        { txt: "注册", current: false, type: 'register' }
+    ],
+    model:'login',
+    ruleForm: {
+        username: '',
+        password: '',
+        passwords: '',
+        code: ''
+    },
+    rules: {
+        username: [
+        { validator: validateUsername, trigger: 'blur' }
+        ],
+        password: [
+        { validator: validatePassword, trigger: 'blur' }
+        ],
+        passwords: [
+        { validator: validatePasswords, trigger: 'blur' }
+        ],
+        code: [
+        { validator: checkCode, trigger: 'blur' }
+        ]
+    }
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    toggleMenu(item) {
+      this.menutab.forEach(element => {
+        element.current = false;
+      });
+      item.current = true;
+      this.model = item.type;
+    },
+    submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
+  },
+  props: {},
+  watch: {}
+};
 </script>
 <style lang="scss" scoped>
-#login{
-    height: 100vh;
-    background-color: #344a5f;
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+}
+.current {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+.login-form{
+    label{
+        display: block;
+        font-size: 14px;
+        color: #fff;
+    }
+}
+.block{
+    display: block;
+    width: 100%;
 }
 </style>
